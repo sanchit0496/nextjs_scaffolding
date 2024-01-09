66dcfc4db8d8dfaec4c0507b4afced5b6b55d1e3
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-01-10 00:44:41 +0530

    Tailwind CSS is added

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/global.css'
+
+const App = () => {
+    return(
+        <div>
+            <div className="p-4 bg-blue-500 text-white">Hello, Tailwind!</div>
+        </div>
+    )
+}
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      
       <main>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
