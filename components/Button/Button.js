3c822246b84b56545fec1b213129fa321c55d0a2
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-01-10 00:56:55 +0530

    creating a helper component

---
+++ components/Button/Button.js
@@ -0,0 +1,12 @@
+const Button = ({ children, onClick }) => {
+    return (
+        <button 
+            onClick={onClick} 
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
+import Button from '../components/Button/Button';
 import '../styles/global.css'
 
 const App = () => {
     return(
         <div>
             <div className="p-4 bg-blue-500 text-white">Hello, Tailwind!</div>
+            <br></br>
+            <Button>Login</Button>
         </div>
     )
 }
